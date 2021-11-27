1d1aee4b673ec85fff20a8ba91cb1226274e7fc4
Sean Killeen <SeanKilleen@gmail.com>
2021-11-27 12:07:47 -0500

    Basic project files

---
+++ SantaSleighCode.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SantaSleighCode.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ SantaSleighCode/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SantaSleighCode
+{
+    public class Class1
+    {
+    }
+}
