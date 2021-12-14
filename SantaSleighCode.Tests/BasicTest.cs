6ff7eb8924e6d739674e4dad21f55fcfed1b1bca
Sean Killeen <SeanKilleen@gmail.com>
2021-12-14 00:13:51 -0500

    first test

---
+++ SantaSleighCode.Tests/BasicTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace SantaSleighCode.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void MathStillWorks()
+        {
+            var result = 1 + 1;
+
+            Assert.Equal(2, result);
+        }
+    }
+}
