fef9df6460c0bec7ed8123cfd98ee1e7f1a0a8a1
Sean Killeen <SeanKilleen@gmail.com>
2021-12-14 14:11:20 -0500

    GetYCoordinate_FacingWestAndMovingBackward_NoChange

--- SantaSleighCode.Tests/SantaSleighTests.cs
+++ SantaSleighCode.Tests/SantaSleighTests.cs
@@ -391,5 +391,21 @@ namespace SantaSleighCode.Tests
 
             result.Should().Be(0);
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(12)]
+        [InlineData(123)]
+        public void GetYCoordinate_FacingWestAndMovingBackward_NoChange(int numberOfSpaces)
+        {
+            var sut = new SantaSleigh();
+            sut.TurnLeft();
+
+            sut.MoveBackward(numberOfSpaces);
+            var result = sut.GetYCoordinate();
+
+            result.Should().Be(0);
+        }
+
     }
 }
